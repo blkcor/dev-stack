7f2616e1e7ab82e09e00fc6a96bbecccf6b1e58d
blkcor <blkcor.dev@gmail.com>
2025-10-07 12:41:38 +0800

    chore: left sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,7 @@
-import { signOut } from '@/auth'
-import { Button } from '@/components/ui/button'
-import ROUTES from '@/constants/routes'
-
 export default function Home() {
   return (
     <>
       <h1 className='h1-bold'>Welcome to next.js project</h1>
-      <form
-        className='px-10 pt-[100px]'
-        action={async () => {
-          'use server'
-          await signOut({
-            redirectTo: ROUTES.SIGN_IN,
-          })
-        }}
-      >
-        <Button type='submit'>Logout</Button>
-      </form>
     </>
   )
 }
---
+++ components/navigation/LeftSidebar.tsx
@@ -0,0 +1,45 @@
+import { Icon } from '@iconify/react'
+import Link from 'next/link'
+import * as React from 'react'
+
+import NavLinks from '@/components/navigation/navbar/NavLinks'
+import { Button } from '@/components/ui/button'
+import ROUTES from '@/constants/routes'
+
+const LeftSidebar = () => {
+  return (
+    <section className='custom-scrollbar background-light900_dark200 light-border shadow-light-300 sticky top-0 left-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 max-sm:hidden lg:w-[266px] dark:shadow-none'>
+      <div className='flex flex-1 flex-col gap-6'>
+        <NavLinks />
+      </div>
+      <div className='flex flex-col gap-3'>
+        <Button
+          className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'
+          asChild
+        >
+          <Link href={ROUTES.SIGN_IN} className='relative right-[5px] flex items-center gap-4'>
+            <Icon
+              icon='material-symbols:login-sharp'
+              className='invert-colors text-dark300_light900 h-5 w-5'
+            />
+            <span className='primary-text-gradient max-lg:hidden'>Log In</span>
+          </Link>
+        </Button>
+        <Button
+          className='small-medium light-borde-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'
+          asChild
+        >
+          <Link href={ROUTES.SIGN_UP} className='flex items-center gap-4'>
+            <Icon
+              icon='bitcoin-icons:sign-outline'
+              className='invert-colors text-dark300_light900 h-5 w-5'
+            />
+            <span className='max-lg:hidden'>Sign Up</span>
+          </Link>
+        </Button>
+      </div>
+    </section>
+  )
+}
+
+export default LeftSidebar
