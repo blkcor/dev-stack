77c603998a59075fc2e2a48bda1bd193e76c9c11
blkcor <blkcor.dev@gmail.com>
2025-10-28 00:29:54 +0800

    chore: add account model

--- database/user.model.ts
+++ database/user.model.ts
@@ -11,7 +11,7 @@ export interface IUser {
   reputation?: number
 }
 
-const userSchema = new Schema(
+const UserSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     username: { type: String, required: true },
@@ -27,6 +27,6 @@ const userSchema = new Schema(
   }
 )
 
-const User = models.user || model<IUser>('User', userSchema)
+const User = models.User || model<IUser>('User', UserSchema)
 
 export default User
